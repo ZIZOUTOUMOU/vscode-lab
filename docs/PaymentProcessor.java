ffc98e597e78645956cb8f4ab7bdb9b2049e6f57
zizoutoumou <aziztid3@gmail.com>
2026-05-03 12:12:00 +0100

    exercise5: add PaymentProcessor before refactoring

---
+++ docs/PaymentProcessor.java
@@ -0,0 +1,13 @@
+public class PaymentProcessor {
+    public void processPayment(String paymentMethod, double amount) {
+        if (paymentMethod.equals("CreditCard")) {
+            System.out.println("Processing credit card payment of $" + amount);
+        } else if (paymentMethod.equals("PayPal")) {
+            System.out.println("Processing PayPal payment of $" + amount);
+        } else if (paymentMethod.equals("Bitcoin")) {
+            System.out.println("Processing Bitcoin payment of $" + amount);
+        } else {
+            throw new IllegalArgumentException("Unknown payment method");
+        }
+    }
+}
