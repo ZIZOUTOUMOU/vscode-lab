1a6b3b5896a9759938770b7029ab5354b1e413c0
zizoutoumou <aziztid3@gmail.com>
2026-05-03 12:09:15 +0100

    exercise3: add Employee before refactoring

---
+++ docs/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    public double calculateBonus(String type) {
+        if (type.equals("Manager")) {
+            return 5000;
+        } else if (type.equals("Developer")) {
+            return 3000;
+        } else {
+            return 1000;
+        }
+    }
+}
